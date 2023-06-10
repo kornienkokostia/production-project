d1776ca0fa5d030313adee8a6ec737d814c42731
kornienkokostia <kornienkokostia@gmail.com>
2023-06-10 16:46:55 +0300

    moved apge links to sidebar and added icons to them

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
@@ -6,14 +6,19 @@ import { useTheme } from 'app/providers/ThemeProvider';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { AppRouter } from './providers/router';
+import { Modal } from 'shared/ui/Modal/Modal';
 
 function App() {
   const { theme } = useTheme();
+
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className={classNames('app', {}, [theme])}>
       <Suspense fallback="">
         <Navbar />
+        {/* <button onClick={() => setIsOpen(true)}>modal</button>
+        <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} /> */}
         <div className="content-page">
           <Sidebar />
           <AppRouter />
