cce705429b4ab29ce681e79a45195d8d7166e199
kornienkokostia <kornienkokostia@gmail.com>
2023-07-10 21:09:13 +0300

    fix request headers

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -3,7 +3,17 @@ import { Page } from 'widgets/Page/Page';
 
 function MainPage() {
   const { t } = useTranslation();
-  return <Page>{t('Main page')}</Page>;
+  return (
+    <div>
+      <h3>Accounts:</h3>
+      <br />
+      <p>username: g-eazy</p>
+      <p>password: 123</p>
+      <br />
+      <p>username: cap</p>
+      <p>password: 123</p>
+    </div>
+  );
 }
 
 export default MainPage;
