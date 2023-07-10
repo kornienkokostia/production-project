0255b399ecd2243f0ee6e0dfb15b3ca9eb500672
kornienkokostia <kornienkokostia@gmail.com>
2023-07-10 21:54:03 +0300

    add accounts info an about link

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { AppLink } from 'shared/ui/AppLink/AppLink';
 import { Page } from 'widgets/Page/Page';
 
 function AboutPage() {
   const { t } = useTranslation();
 
-  return <Page>{t('About page')}</Page>;
+  return (
+    <div>
+      <a
+        href={'https://github.com/kornienkokostia/production-project'}
+        target={'_blank'}>
+        Git Hub Repo
+      </a>
+    </div>
+  );
 }
 
 export default AboutPage;
