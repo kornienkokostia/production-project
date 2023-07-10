28204493bc588676cfe4cbf60e7fa379df14c0e0
kornienkokostia <kornienkokostia@gmail.com>
2023-07-10 20:31:03 +0300

    add redirects file again

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -25,6 +25,7 @@ export function buildPlugins({
     new CopyPlugin({
       patterns: [
         { from: paths.locales, to: paths.buildLocales },
+        { from: paths.redirects, to: paths.buildRedirects },
         { from: paths.favIcon, to: paths.buildFavIcon },
       ],
     }),
