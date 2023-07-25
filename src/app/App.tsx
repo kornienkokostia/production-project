d4ce775db9e017658032fde383da0ea9130540ae
kornienkokostia <kornienkokostia@gmail.com>
2023-07-25 11:55:14 +0300

    fix height

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,7 +6,7 @@ import React, {
   useState,
 } from 'react';
 import { useSelector } from 'react-redux';
-import { isDesktop } from 'react-device-detect';
+import { isDesktop, isMobileSafari } from 'react-device-detect';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
 import { Navbar } from '@/widgets/Navbar';
@@ -48,7 +48,11 @@ function App() {
         <div
           className={classNames(
             'content-page',
-            { full: navbarCollapsed, notDesktop: !isDesktop },
+            {
+              full: navbarCollapsed,
+              notDesktop: !isDesktop,
+              isMobileSafari: isMobileSafari,
+            },
             [],
           )}>
           {isDesktop && <Sidebar />}
